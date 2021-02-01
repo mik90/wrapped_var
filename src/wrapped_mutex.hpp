9683546201a5b4c6d2891ce823ea692c2a3cb1ae
Mike Kaliman <kaliman.mike@gmail.com>
2021-01-31 21:45:09 -0500

    Impl RAII accessor, update clang tidy

--- src/wrapped_mutex.hpp
+++ src/wrapped_mutex.hpp
@@ -1,14 +1,42 @@
 #pragma once
 
+#include <mutex>
 #include <utility>
 
 namespace mik {
-template <class UnderlyingVarType>
-class WrappedMutex {
+
+/**
+ * @brief RAII style accessor for a WrappedMutex var
+ */
+template <class UnderlyingVarType, class MutexType>
+class VarWithLock {
 public:
-  explicit WrappedMutex(UnderlyingVarType var) : var_(std::move(var)) {}
+  VarWithLock(UnderlyingVarType var, std::unique_lock<MutexType> lock)
+      : var_(std::move(var)), lock_(std::move(lock)) {}
+
+  UnderlyingVarType& get_ref() noexcept { return var_; }
+
+  const UnderlyingVarType& get_ref() const noexcept { return var_; }
+
+  UnderlyingVarType clone() const { return var_; }
 
 private:
   UnderlyingVarType var_;
+  std::unique_lock<MutexType> lock_;
 };
+
+/**
+ * @brief Mutex wrapper around variable of type UnderlyingVarType
+ */
+template <class UnderlyingVarType, class MutexType = std::mutex>
+class WrappedMutex {
+public:
+  explicit WrappedMutex(UnderlyingVarType var) : var_(std::move(var)) {}
+  VarWithLock<UnderlyingVarType, MutexType> get() { return {var_, std::unique_lock{mutex_}}; }
+
+private:
+  UnderlyingVarType var_;
+  MutexType mutex_;
+};
+
 } // namespace mik
--- test/test_mutex.cpp
+++ test/test_mutex.cpp
@@ -1,11 +1,41 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <mutex>
+#include <string>
+
 #include "wrapped_mutex.hpp"
 
 using namespace mik;
 
+// Check that it even compiles
 TEST(WrappedMutexTest, intConstructor) {
-  // Check that it event compiles
-  ASSERT_NO_THROW(WrappedMutex<int> wrappedInt{3});
+  WrappedMutex<int> wrappedInt{3};
+  auto var_with_lock = wrappedInt.get();
+  ASSERT_EQ(var_with_lock.get_ref(), 3);
 }
+
+TEST(WrappedMutexTest, recursiveMutex) {
+  WrappedMutex<int, std::recursive_mutex> wrappedInt{3};
+  auto var_with_lock = wrappedInt.get();
+
+  ASSERT_EQ(var_with_lock.get_ref(), 3);
+}
+
+TEST(WrappedMutexTest, userDefinedType) {
+  struct UserDefinedStruct {
+    int value_0;
+    float value_1;
+    std::string value_3;
+  };
+
+  UserDefinedStruct udt{0, 0.0f, "hello"};
+  WrappedMutex<UserDefinedStruct> wrappedUdt{udt};
+}
+
+TEST(WrappedMutexTest, templateDeduction) {
+  std::string a_string{"hello"};
+  WrappedMutex wrappedString{a_string};
+}
+
+/// @todo Make tests that attempt to access the mutex-protected underyling var
